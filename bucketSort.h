83f53a289511d9a48c58520d07eeece46632a9e2
ImL1s <aa22396584@gmail.com>
2019-10-29 18:40:59 +0800

    first commit

---
+++ bucketSort.h
@@ -0,0 +1,10 @@
+//
+// Created by iml1s on 2019/10/29.
+//
+
+#ifndef ALGORITHM_BUCKETSORT_H
+#define ALGORITHM_BUCKETSORT_H
+
+int bucketSort();
+
+#endif //ALGORITHM_BUCKETSORT_H
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "bucketSort.h"
+
+int main() {
+    printf("Hello, World!\n");
+    bucketSort();
+    return 0;
+}
